871b49ec7ce017fac82fa6ea3c3ed95bd78064b0
Casey Herold <caseynate@gmail.com>
2017-06-27 13:41:20 -0600

    name input works

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-// var $baseURL = "http://jservice.io/"
-// $(document).ready(function(){$.get($baseURL)
-//   .then(function(){
-//     console.log(data)
-//   })
+var $baseURL = "http://jservice.io/api/clues/"
+
+$(window).on('load',function(){
+    $('#myModal').modal('show');
+});
+
+
+$('.submitName').click(function (event) {
+  event.preventDefault()
+  var $playerName = $('.nameInput').val()
+  $('#player').append($playerName)
+})
+$('.nameInput').keyup(function(e){
+  e.preventDefault()
+  if(e.which == 13) {
+    $('.submitName').click();
+  }
+});
+
+$(document).ready(function() {
+    $.get($baseURL).then(function(data) {
+    })
+})
