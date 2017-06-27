494268d8fa3535b5619798dadd0d663dab162852
Casey Herold <caseynate@gmail.com>
2017-06-27 16:10:16 -0600

    Randomly generates categories

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var $baseURL = "http://jservice.io/api/clues/"
+var $category = "http://jservice.io/api/category?id="
 
 $(window).on('load',function(){
     $('#myModal').modal('show');
@@ -18,6 +19,34 @@ $('.nameInput').keyup(function(e){
 });
 
 $(document).ready(function() {
-    $.get($baseURL).then(function(data) {
+    $.get($category + Math.floor((Math.random() * 18000) + 1)).then(function(data) {
+      $('.catOne').append(data.title)
+    })
+    $.get($category + Math.floor((Math.random() * 18000) + 1)).then(function(data) {
+    $('.catTwo').append(data.title)
+    })
+    $.get($category + Math.floor((Math.random() * 18000) + 1)).then(function(data) {
+    $('.catThree').append(data.title)
+    })
+    $.get($category + Math.floor((Math.random() * 18000) + 1)).then(function(data) {
+    $('.catFour').append(data.title)
+    })
+    $.get($category + Math.floor((Math.random() * 18000) + 1)).then(function(data) {
+    $('.catFive').append(data.title)
     })
 })
+
+$('.firstCategory').click(function (event) {
+  event.preventDefault()
+  $('#myAnswerModal').modal('show');
+})
+
+$('.submitAnswer').click(function (event) {
+  event.preventDefault()
+})
+$('.answerInput').keyup(function(e){
+  e.preventDefault()
+  if(e.which == 13) {
+    $('.submitAnswer').click();
+  }
+});
